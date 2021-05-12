68b9cdd74c1afdfbf58bf4ca028c1d07d3775c6e
Geoxing32 <78418474+Geoxing32@users.noreply.github.com>
2021-05-12 16:52:02 +0200

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,20 @@
+const QUOTEBANK = [{
+       quote: "All men are ordinary only the extrordinary men can see that",
+	author: "MDB"},
+{
+       quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+	author: "Napoleon Hill"},
+{
+       quote: "Strive not to be a success, but rather to be of value.",
+	author: "Albert Einstein"},
+{
+       quote: "La valeur d'un actif est proportionnellement et directement lié  à l'impact qu'il apporte dans  le monde",
+	author: "Chicken Genius"},
+{
+       quote: " Aucun arbre ne pourra pousser jusqu'au paradis sans plonger ses racines dans les  profondeurs de l'enfer.",
+	author: "Carl Jung"},
+{
+       quote: "Une erreur ne tue pas mais l'accumulation d'erreurs tue.",
+	author: "Geovany Silva "},
+
+]
