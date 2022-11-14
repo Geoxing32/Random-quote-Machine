517642c50f5cd3fb4a9a69a478cbd834b0573772
Geoxing32 <78418474+Geoxing32@users.noreply.github.com>
2022-11-14 17:45:21 +0100

    Update quote.js

--- quote.js
+++ quote.js
@@ -16,5 +16,93 @@ const QUOTEBANK = [{
 {
        quote: "Une erreur ne tue pas mais l'accumulation d'erreurs tue.",
 	author: "Geovany Silva "},
+		   
+{	   quote: "Tout les hommes sont ordinaires seul les hommes extraordinaires peuvent le voir.",
+	author: "Geovany Silva "},
+		
+{		 quote: "On est fait de chair mais on doit vivre comme si on était fait en fer.",
+	author: "Geovany Silva "},
+		
+		
+{		quote: "La langue est minuscule mais peut causer d'énormes dégâts.",
+	author: "Geovany Silva "},
+		
+		
+{		quote: "Celui qui agit échoue,celui qui s'attache à une chose la perd,de la vient le Saint que n'agit pas,c'est pourquoi il n'echoue point.il ne s attache à rien , c est pourquoi il ne perd point.",
+	author: "Geovany Silva "},
+		
+		
+{		quote: "Ce que tu vise détermine ce que tu vois.",
+	author: "Geovany Silva "},
+		
+{		quote: "L'émotion est l'ennemi de la paix intérieur et de la raison.",
+	author: "Geovany Silva "},
+		
+{		quote: "Soit prêt comme ça tu n'aura pas a te préparé.",
+	author: "Geovany Silva "},
+		
+{		quote: "Le mensonge tue la confiance et te prive personnes en lequel tu peux vraiment  compter,  encore pire le mensonge t'empêche  de te voir telle que tu es réellement.",
+	author: "Geovany Silva "},
+		
+{		quote: "You cannot  honor God and dishonor his commandements.",
+	author: "Geovany Silva "},
+		
+{		quote: "Si l'oiseau  peut trouver à manger l’homme  trouvera à manger.",
+	author: "Geovany Silva "},
+		
+{		quote: "La mémoire ne sert pas a se souvenir du passé mais a préparer l'avenir .",
+	author: "Jordan B.Peterson"},
+		
+{		quote: "Vivre Sainement devrait être une norme, le fait que ça ne soit pas vu comme normal  prouve que les systèmes éducatifs sont obsolètes.",
+	author: "Geovany Silva "},
+		
+{		quote: "Nunka ka bu skeci di undi ki bu bem pa bu sabi undi ki bu ata bai.",
+	author: "Geovany Silva "},
+				
+{		quote: "Honoring God it's valuing people",
+	author: "Geovany Silva "},
+				
+{		quote: "Un Orateur/Leader ne rentre pas des idées dans la tête de son auditoire, il ne fait que  mettre en Valeur les idées que son auditoire a déjà  dans la tête.",
+	author: "Geovany Silva "},
+		
+{		quote: "Tout les hommes sont ordinaires seul les hommes extraordinaires peuvent le voir.",
+	author: "Geovany Silva "},
+		
+{		quote: "Il est écrit: L'homme ne vivra pas de pain seulement, mais de toute parole qui sort de la bouche de Dieu",
+	author: "Mathieu 4:4 "},
+		   
+{		   quote: "La pauvrété mentale pousse à montrer ce qu'on a dans le but de cacher ce qu'on a pas.",
+	author: "Geovany Silva "},
+		   
+{	
+	          quote: "La richesses se trouve dans les investissement.",
+	author: "Geovany Silva "},
+		   
+		   
+{		    quote: "L'amour est patient, il est plein de bonté; l'amour n'est pas envieux; l'amour ne se vante pas, il ne s'enfle pas d'orgueil, il ne fait rien de malhonnête, il ne cherche pas son intérêt, il ne s'irrite pas, il ne soupçonne pas le mal",
+	author: "1 Corinthiens 13:4-5 "},
+		   
+		   	   
+{		    quote: " Un peu de sommeil, un peu d'assoupissement, Un peu croiser les mains pour dormir!... Et la pauvreté te surprendra, comme un rôdeur, Et la disette, comme un homme en armes.",
+	author: " Proverbes 24-33-34 "},
+		   
+		   
+		   	   
+{		    quote: "Il n'y a qu'un pas entre l'Amour et la Haine ",
+	author: "Geovany Silva "},
+		   
+		   
+	
+		   
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
 
 ]
